a7fa901a129a0ae26c2d8e688e266b32ccf202e0
Anton Popovine <anton.popovine@gmail.com>
2023-03-13 22:36:25 +0000

    fix run.py

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    address = request.args.get('address')
+    address = request.args.get('address', 'not_provided')
     return "Getting smart contract for {}!".format(address)
 
 if __name__ == "__main__":
