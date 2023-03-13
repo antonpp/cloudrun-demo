75a10da917fa8e33b81539419b9a63080199a61a
Anton Popovine <anton.popovine@gmail.com>
2023-03-13 22:31:59 +0000

    check address

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Antler: Hej {}!".format(name)
+    address = request.args.get('address')
+    return "Getting smart contract for {}!".format(address)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
